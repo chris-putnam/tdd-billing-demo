f3eb620877b8cc697af76571ca4430f0cdbd2730
Chris Putnam <chris@christopher-putnam.com>
2016-03-25 14:21:11 -0400

    add empty projects

---
+++ TddBillingDemo.Tests/BillingDoohickeyTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddBillingDemo.Tests
+{
+    class BillingDoohickeyTests
+    {
+    }
+}
