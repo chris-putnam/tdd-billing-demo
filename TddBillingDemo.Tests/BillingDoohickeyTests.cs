8b09b81ef55846133d2dd2b8671240c637bf39be
Chris Putnam <chris@christopher-putnam.com>
2016-03-25 14:38:50 -0400

    stub out some things for first test

--- TddBillingDemo.Tests/BillingDoohickeyTests.cs
+++ TddBillingDemo.Tests/BillingDoohickeyTests.cs
@@ -11,23 +11,52 @@ namespace TddBillingDemo.Tests
     class BillingDoohickeyTests
     {
         [Fact]
-        public void Monkey()
+        public void CustomerWhoDoesNotHaveSubscriptionDoesNotGetCharged()
         {
             // just start writing your test without thinking too hard about the implementation
 
             // we need a source for customers
             // we need a service to charge customers
 
-            ICustomerRepository repo = new Mock<ICustomerRepository>();
-            ICreditCardCharger charger = new Mock<ICreditCardCharger>();
+            var repo = new Mock<ICustomerRepository>();
+            var charger = new Mock<ICreditCardCharger>();
+            var customer = new Customer();
 
-            BillingDoohickey thing = new BillingDoohickey();
+            BillingDoohickey thing = new BillingDoohickey(repo.Object, charger.Object);
             thing.ProcessMonth(2001, 8);
+
+            charger.Verify(c => c.ChargeCustomer(customer), Times.Never());
         }
         //Monthly billing
         //Grace period for missed payments ("dunning" status)
         //Not all customers are necessarily subscribers
         //Idle customers should be automatically unsubscribed
+    }
+
+    public class BillingDoohickey
+    {
+        private ICreditCardCharger _charger;
+        private ICustomerRepository _repo;
+
+        public BillingDoohickey(ICustomerRepository repo, ICreditCardCharger charger)
+        {
+            _repo = repo;
+            _charger = charger;
+        }
+
+        internal void ProcessMonth(int year, int month)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface ICreditCardCharger
+    {
+        void ChargeCustomer(Customer customer);
+    }
+
+    public interface ICustomerRepository
+    {
     }
 
     public class Customer
