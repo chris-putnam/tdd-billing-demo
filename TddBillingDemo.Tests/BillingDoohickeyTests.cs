2bb2c5ff60c4a2067754ee1757d548c294d5de15
Chris Putnam <chris@christopher-putnam.com>
2016-03-25 14:26:38 -0400

    add notes for tests

--- TddBillingDemo.Tests/BillingDoohickeyTests.cs
+++ TddBillingDemo.Tests/BillingDoohickeyTests.cs
@@ -8,5 +8,9 @@ namespace TddBillingDemo.Tests
 {
     class BillingDoohickeyTests
     {
+        //Monthly billing
+        //Grace period for missed payments ("dunning" status)
+        //Not all customers are necessarily subscribers
+        //Idle customers should be automatically unsubscribed
     }
 }
