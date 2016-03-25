9001fc16e9eead24622bb24643859c8831350d3d
Chris Putnam <chris@christopher-putnam.com>
2016-03-25 14:30:11 -0400

    start writing first test

--- TddBillingDemo.Tests/BillingDoohickeyTests.cs
+++ TddBillingDemo.Tests/BillingDoohickeyTests.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace TddBillingDemo.Tests
 {
     class BillingDoohickeyTests
     {
+        [Fact]
+        public void Monkey()
+        {
+            // just start writing your test without thinking too hard about the implementation
+            BillingDoohickey thing = new BillingDoohickey();
+            thing.ProcessMonth(2001, 8);
+        }
         //Monthly billing
         //Grace period for missed payments ("dunning" status)
         //Not all customers are necessarily subscribers
         //Idle customers should be automatically unsubscribed
+    }
+
+    public class Customer
+    {
+        
     }
 }
